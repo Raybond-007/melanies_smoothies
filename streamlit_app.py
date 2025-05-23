f838489cdd4c437e7c7b627e8e070b550abb61c0
Raybond-007 <raybond007@hotmail.co.uk>
2025-05-23 17:37:18 +0100

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 
 st.title(f":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
 st.write("""Orders that need to be filled.""")
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED") == False)
 
 # Check if there are pending orders before rendering components
